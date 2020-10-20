02dba4471c1128b5b46a8cf493339c1010c4cddf
Nicolas Acerbi <50033057+AcerbiNicolas@users.noreply.github.com>
2020-10-20 20:15:11 -0300

    First Commit
    MVP (minimum viable product) is complete.

---
+++ CapsCheck/CapsCheck/CapsLock Check/src/Main.java
@@ -0,0 +1,109 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static boolean running = true;
+    private static boolean isActive = false;
+    static JFrame  green;
+
+
+
+
+    public static void main(String[] args) throws NativeHookException {
+
+
+        Toolkit kit = Toolkit.getDefaultToolkit();
+
+        isActive = kit.getLockingKeyState(KeyEvent.VK_CAPS_LOCK);   //Check caps when application is run
+
+        Dimension screenSize = kit.getScreenSize();
+        int width =(int) screenSize.getWidth();
+        int heigt =(int) screenSize.getHeight();
+
+        JFrame information = new JFrame();
+        information.setBounds(width / 4, heigt / 4, width / 2, heigt / 2);
+        information.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
+        information.setVisible(true);
+
+
+
+
+
+        Logger l = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        l.setLevel(Level.OFF);
+
+
+        GlobalScreen.registerNativeHook();
+        GlobalScreen.addNativeKeyListener(listener);
+
+        while(running){
+            try {
+                changeWindowOptions(information);
+                Thread.sleep(300);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        GlobalScreen.unregisterNativeHook();
+
+    }
+
+
+    public static void changeWindowOptions(JFrame info){
+        Taskbar taskbar = Taskbar.getTaskbar();
+
+
+        taskbar.setWindowProgressValue(info, 100);
+
+        if(isActive){
+            info.setTitle("CapsLock is: ON");
+            taskbar.setWindowProgressState(info, Taskbar.State.ERROR);
+        }else{
+            info.setTitle("CapsLock is: OFF");
+            taskbar.setWindowProgressState(info, Taskbar.State.OFF);
+        }
+    }
+
+    private static NativeKeyListener listener = new NativeKeyListener() {
+        @Override
+        public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
+
+        }
+
+        @Override
+        public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
+
+            if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_CAPS_LOCK){
+
+                if (isActive){
+                    isActive = false;
+                }else{
+                    isActive = true;
+                }
+            }
+        }
+
+        @Override
+        public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+
+        }
+    };
+
+
+
+
+
+}
---
+++ CapsCheck/CapsCheck/README.md
@@ -0,0 +1,2 @@
+# CapsCheck
+ If you do not have the keyboard light that tells you if the capslock key is active or not, CapsCheck shows it to you through its light in the windows taskbar
